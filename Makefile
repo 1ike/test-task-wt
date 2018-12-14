10a184a90a3e17608167af58b8bb8a933a764f8e
1ike <googlohren@gmail.com>
2018-12-14 17:52:50 +0300

    Let's go cats!

---
+++ Makefile
@@ -0,0 +1,10 @@
+build:
+	npm run build
+
+b: build
+
+server:
+	npm run server -- --open 'C:\Program Files\Firefox Developer Edition\Firefox.exe'
+
+s: server
+
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const mode = process.env.NODE_ENV || "development";
+
+module.exports = {
+  mode: process.env.NODE_ENV || "development",
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/dist"
+  },
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+
+  devServer: {
+    // contentBase: path.join(__dirname, 'dist'),
+  },
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader"
+      },
+
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
+      }
+    ]
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: "index.html",
+      title: "WebTouch test job",
+      lang: "en",
+      meta: {
+        'viewport': 'width=device-width, initial-scale=1, shrink-to-fit=no',
+        // Will generate: <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        'theme-color': '#4285f4'
+        // Will generate: <meta name="theme-color" content="#4285f4">
+      }
+    })
+  ]
+
+  // When importing a module whose path matches one of the following, just
+  // assume a corresponding global variable exists and use that instead.
+  // This is important because it allows us to avoid bundling all of our
+  // dependencies, which allows browsers to cache those libraries between builds.
+  // externals: {
+  //     "react": "React",
+  //     "react-dom": "ReactDOM"
+  // }
+};
