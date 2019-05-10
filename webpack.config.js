e15bedb128a622b82f8ed05ab86ef1416ab8d21b
1ike <googlohren@gmail.com>
2019-05-10 17:33:28 +0300

    Clean dependencies.

--- webpack.config.js
+++ webpack.config.js
@@ -39,10 +39,7 @@ const common = {
         test: /\.js$/,
         loader: 'source-map-loader'
       },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
+
       {
         test: /\.pug$/,
         loader: 'pug-loader',
@@ -50,15 +47,6 @@ const common = {
     ]
   },
 
-
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
-  // externals: {
-  //     'react': 'React',
-  //     'react-dom': 'ReactDOM'
-  // }
 };
 
 const client = {
@@ -109,8 +97,5 @@ module.exports = () => {
     ...client,
   };
 
-
   return [clientConfig, serverConfig]
-  // return serverConfig
-  // return clientConfig
 };
